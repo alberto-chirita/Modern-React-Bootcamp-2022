d790fa89c3ca342890ba08873e8d078f20c98e5b
Alberto Chirita <alberto.marian.chirita@gmail.com>
2022-04-12 20:14:49 +0300

    Day 42

---
+++ events_demo/src/App.js
@@ -0,0 +1,17 @@
+import WiseSquare from "./WiseSquare";
+import AnnoyingForm from "./AnnoyingForm";
+import CopyDemo from "./CopyDemo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Events!!</h1>
+      <WiseSquare />
+      <AnnoyingForm />
+      <CopyDemo />
+    </div>
+  );
+}
+
+export default App;
