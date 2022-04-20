aafa59b6e6c45fcaa38a336dfd3c00d6cec868d9
Alberto Chirita <alberto.marian.chirita@gmail.com>
2022-04-20 19:47:43 +0300

    Day 48

--- router_intro/src/App.js
+++ router_intro/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
 import Dog from "./Dog";
@@ -11,10 +11,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="App-nav">
+          <NavLink
+            className={({ isActive }) => (isActive ? " active-link" : "")}
+            to="/dog"
+          >
+            Dog
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? " active-link" : "")}
+            to="/dog/c"
+          >
+            Dog(c)
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? " active-link" : "")}
+            to="/dog/r"
+          >
+            Dog(r)
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? " active-link" : "")}
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? " active-link" : "")}
+            to="/"
+          >
+            About
+          </NavLink>
+        </nav>
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/dog" element={<Dog />} />
+          <Route path="/dog" element={<Dog name="Muffins" />} />
+          <Route path="/dog/c" element={<Dog name="Muffins" />} />
+          <Route path="/dog/r" element={<Dog name="Biscuits" />} />
           <Route path="/dog/hater" element={<Hater />} />
         </Routes>
       </div>
--- router_intro/src/Dog.js
+++ router_intro/src/Dog.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import "./Dog.css";
 
 export class Dog extends Component {
+  componentDidMount() {
+    console.log("DOG DID MOUNT!");
+  }
+
+  componentWillUnmount() {
+    console.log("DOG WILL UNMOUNT!");
+  }
+
   render() {
+    console.log("DOG RENDER!");
     return (
       <div className="Dog">
         <h1>Dog!!!</h1>
+        <h3>This dog is named: {this.props.name}</h3>
         <img
           src="https://www.ctvnews.ca/polopoly_fs/1.5277048.1611428206!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg"
           alt="pug"
